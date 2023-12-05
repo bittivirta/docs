af50636fd617397da63ff39fd449354612c5e9b4
korhox <juuso.korhonen@bittivirta.fi>
2023-12-05 21:21:10 +0200

    feat: :sparkles: Update homepage

--- src/components/CategoryIndex/index.jsx
+++ src/components/CategoryIndex/index.jsx
@@ -2,32 +2,33 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faQuestionCircle } from '@fortawesome/pro-solid-svg-icons';
 import Link from '@docusaurus/Link';
+import clsx from 'clsx';
 
 
-function Category({ link, title, description, icon, image }) {
+function Category({ link, title, description, icon, image, variation }) {
   let visual = <FontAwesomeIcon icon={faQuestionCircle} size='2x' fixedWidth />
   if (icon) {
-    visual = <FontAwesomeIcon icon={icon} size='2x' fixedWidth />
+    visual = <FontAwesomeIcon icon={icon} size={variation == 'large' ? '4x' : '2x'} fixedWidth />
   } else if (image) {
-    visual = <img src={image} alt={title} width={40} className='block' />
+    visual = <img src={image} alt={title} width={variation == 'large' ? '40' : '48'} className='block' />
   }
   return (
-    <Link to={link} className="shadow-md border border-solid border-slate-100 p-4 rounded-xl flex items-center text-slate-800 hover:no-underline hover:text-primary hover:scale-[1.02] transition-all">
-      <div className='mr-3'>
+    <Link to={link} className={clsx("shadow-md bg-white border border-solid border-slate-100 p-4 rounded-xl flex items-center text-slate-800 hover:no-underline hover:text-primary hover:scale-[1.02] transition-all", variation == "large" ? "p-6" : "p-4")}>
+      <div className={clsx(variation == "large" ? "mr-5" : "mr-4")}>
         {visual}
       </div>
       <div>
-        <h3 className="m-0 p-0">{title}</h3>
-        {description && <span className="m-0 p-0">{description}</span>}
+        <h3 className={clsx("m-0 p-0", variation == "large" && "text-2xl")}>{title}</h3>
+        {description && <span className="m-0 p-0 font-medium">{description}</span>}
       </div>
     </Link>
   );
 }
 
-export default function CategoryIndex({ items }) {
+export default function CategoryIndex({ items, cols = 2, variation }) {
   return (
     <section>
-      <div className="grid grid-cols-2 gap-3">
+      <div className={clsx("grid gap-3", cols == 2 && `md:grid-cols-2`, cols == 3 && `md:grid-cols-3`)}>
         {items.map((item, index) => (
           <Category
             key={index}
@@ -36,6 +37,7 @@ export default function CategoryIndex({ items }) {
             description={item.description}
             icon={item.icon}
             image={item.image}
+            variation={variation}
           />
         ))}
       </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,68 @@
 import React from "react";
-import clsx from "clsx";
-import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import CategoryIndex from '@site/src/components/CategoryIndex';
 
-import styles from "./index.module.css";
 
 function HomepageHeader() {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<header className={clsx("hero hero--primary", styles.heroBanner)}>
-			<div className="container">
-				<h1 className="hero__title">{siteConfig.title}</h1>
-				<p className="hero__subtitle">{siteConfig.tagline}</p>
-			</div>
-		</header>
-	);
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <header className="hero -mt-5 bg-waves-animated bg-cover">
+      <div className="container py-10">
+        <h1 className="text-6xl text-white">{siteConfig.title}</h1>
+      </div>
+    </header>
+  );
 }
 
 export default function Home() {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<Layout description="Bittivirta Docs is the help article database for Bittivirta Services.">
-			<HomepageHeader />
-			<main>
-				<HomepageFeatures />
-			</main>
-		</Layout>
-	);
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout description="Bittivirta Docs is the help article database for Bittivirta Services.">
+      <HomepageHeader />
+      <main>
+        <div className="container py-20">
+          <h2 className="text-3xl">Valitse aihe, josta kaipaat lisätietoa</h2>
+          <CategoryIndex cols={3} variation="large" items={[
+            {
+              "link": "/client-area",
+              "icon": ["fat", "users"],
+              "title": "Asiakasalue",
+              "description": "Asiakasalueen käyttöohjeet"
+            },
+            {
+              "link": "/hosting",
+              "icon": ["fat", "hotel"],
+              "title": "Webhotelli",
+              "description": "Webhotellien käyttöohjeet"
+            },
+            {
+              "link": "/servers",
+              "icon": ["fat", "usb-drive"],
+              "title": "Virtuaalipalvelimet",
+              "description": "Virtuaalimien käyttöohjeet"
+            },
+            {
+              "link": "/game",
+              "icon": ["fat", "gamepad-alt"],
+              "title": "Pelipalvelimet",
+              "description": "Pelipalvelinten käyttöohjeet"
+            },
+            {
+              "link": "/docs",
+              "icon": ["fat", "book"],
+              "title": "Dokumentaatio",
+              "description": "Dokumentaation käyttöohjeet"
+            },
+            {
+              "link": "/company",
+              "icon": ["fat", "building"],
+              "title": "Yritys",
+              "description": "Tietoa Bittivirrasta"
+            },
+          ]} />
+        </div>
+      </main>
+    </Layout>
+  );
 }
