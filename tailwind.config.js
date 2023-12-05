1edd2161071d605761823551040da6d448ede704
korhox <juuso.korhonen@bittivirta.fi>
2023-12-05 13:33:14 +0200

    feat: :sparkles: Update colors, shadows and add backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,28 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'primary': '#1145ca',
-      }
+        primary: { DEFAULT: '#1145ca', 50: '#F8F9FC', 100: '#ECEEF4', 200: '#DBDEE6', 300: '#BBC6E3', 400: '#1854EC', 500: '#1145ca', 600: '#0D3396', 700: '#082263', 800: '#1d274b', 900: '#051a2e' },
+        accent: "#143a9b",
+        obsidian: "#051a2e",
+        argon: "#f8fafc",
+        pixel: "#1e3449",
+        cloud: "#d8e7f3",
+        info: "#4488e2",
+        success: "#0cc46b",
+        warning: "#e8ad0b",
+        danger: "#d63220",
+      },
+      backgroundImage: {
+        waves: "url('https://cdn.bittivirta.fi/brand/background/waves/svg/blue.svg')",
+        "waves-animated": "url('https://cdn.bittivirta.fi/brand/background/waves/svg/animated/blue.svg')"
+      },
+      boxShadow: {
+        xs: "0 1px 1px rgba(0,0,0,.05)",
+        sm: "0px 0px 1px rgba(0, 0, 0, 0.1), 0px 2px 16px rgba(0, 0, 0, 0.08)",
+        md: "0px 0px 1px rgba(0, 0, 0, 0.1), 0px 2px 24px rgba(0, 0, 0, 0.08)",
+        lg: "0px 0px 1px rgba(0, 0, 0, 0.12), 0px 8px 32px rgba(0, 0, 0, 0.08)",
+        xl: "0px 0px 1px rgba(0, 0, 0, 0.12), 0px 16px 40px rgba(0, 0, 0, 0.16)",
+      },
     },
   },
   plugins: [],
