a4e13198713d2f2978d527001db4a4b39d1d7c5c
korhox <juuso.korhonen@bittivirta.fi>
2024-03-27 00:45:39 +0200

    fix: :bug: Fix Crisp and GA initialization

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Layout from '@theme-original/Layout';
 
 import { Crisp } from "crisp-sdk-web";
 import ReactGA from "react-ga4";
 
-Crisp.configure("0c6dfde1-978e-4f29-959f-9a9886863e4f");
-ReactGA.initialize("G-C7W0T4C3ZR");
 
 export default function LayoutWrapper(props) {
+  useEffect(() => {
+    Crisp.configure("0c6dfde1-978e-4f29-959f-9a9886863e4f");
+    ReactGA.initialize("G-C7W0T4C3ZR");
+  }, []);
+
   return (
     <>
       <Layout {...props} />
