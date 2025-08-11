5b7a3be79595b255c8f3726fdbc9fb63fbec1deb
Juuso Korhonen <juuso.korhonen@bittivirta.fi>
2025-08-11 14:34:57 +0300

    fix: :bug: standardize formatting in redirects configuration

--- redirects.config.js
+++ redirects.config.js
@@ -1,96 +1,114 @@
 const redirects = [
   {
-    "from": ["/en/game/gamepanel/file-management.html", "/fi/game/gamepanel/file-management.html"],
-    "to": "/game/general/file-management"
+    from: [
+      "/en/game/gamepanel/file-management.html",
+      "/fi/game/gamepanel/file-management.html",
+    ],
+    to: "/game/general/file-management",
   },
   {
-    "from": "/en/game/minecraft/domain-plesk.html",
-    "to": "/game/minecraft/custom-domain/domain-plesk"
+    from: "/en/game/minecraft/domain-plesk.html",
+    to: "/game/minecraft/custom-domain/domain-plesk",
   },
   {
-    "from": ["/en", "/fi"],
-    "to": "/"
+    from: ["/en", "/fi"],
+    to: "/",
   },
   {
-    "from": "/en/game/rust/oxidemod-permissions.html",
-    "to": "/game/rust/oxidemod/permissions"
+    from: "/en/game/rust/oxidemod-permissions.html",
+    to: "/game/rust/oxidemod/permissions",
   },
   {
-    "from": ["/en/vps/windows", "/fi/vps/windows"],
-    "to": "/server/vps/windows/"
+    from: ["/en/vps/windows", "/fi/vps/windows"],
+    to: "/server/vps/windows/",
   },
   {
-    "from": ["/en/vps/ubuntu-debian", "/fi/vps/ubuntu-debian"],
-    "to": "/server/vps/linux/"
+    from: ["/en/vps/ubuntu-debian", "/fi/vps/ubuntu-debian"],
+    to: "/server/vps/linux/",
   },
   {
-    "from": ["/asiakasalue/maksaminen/etukateismaksu","/general/client-area/portal/invoicing/prepayments"],
-    "to": "/general/portal/invoicing/prepayments"
+    from: [
+      "/asiakasalue/maksaminen/etukateismaksu",
+      "/general/client-area/portal/invoicing/prepayments",
+    ],
+    to: "/general/portal/invoicing/prepayments",
   },
   {
-    "from": ["/en/docs", "/fi/docs"],
-    "to": "/docs/"
+    from: ["/en/docs", "/fi/docs"],
+    to: "/docs/",
   },
   {
-    "from": ["/en/web", "/fi/web"],
-    "to": "/hosting/web/"
+    from: ["/en/web", "/fi/web"],
+    to: "/hosting/web/",
   },
   {
-    "from": ["/en/company", "/fi/company"],
-    "to": "/company/"
+    from: ["/en/company", "/fi/company"],
+    to: "/company/",
   },
   {
-    "from": ["/en/dedicated", "/fi/dedicated"],
-    "to": "/server/vps/"
+    from: ["/en/dedicated", "/fi/dedicated"],
+    to: "/server/vps/",
   },
   {
-    "from": ["/en/game", "/fi/game"],
-    "to": "/game/"
+    from: ["/en/game", "/fi/game"],
+    to: "/game/",
   },
   {
-    "from": ["/en/game/gamepanel", "/fi/game/gamepanel"],
-    "to": "/game/general/"
+    from: ["/en/game/gamepanel", "/fi/game/gamepanel"],
+    to: "/game/general/",
   },
   {
-    "from": ["/en/domain", "/fi/domain"],
-    "to": "/hosting/web/"
+    from: ["/en/domain", "/fi/domain"],
+    to: "/hosting/web/",
   },
   {
-    "from": ["/en/client-area", "/fi/client-area","/general/client-area/"],
-    "to": "/general/"
+    from: ["/en/client-area", "/fi/client-area", "/general/client-area/"],
+    to: "/general/",
   },
   {
-    "from": ["/en/client-area/invoicing", "/fi/client-area/invoicing","/general/client-area/portal/invoicing/"],
-    "to": "/general/portal/invoicing/"
+    from: [
+      "/en/client-area/invoicing",
+      "/fi/client-area/invoicing",
+      "/general/client-area/portal/invoicing/",
+    ],
+    to: "/general/portal/invoicing/",
   },
   {
-    "from": ["/en/client-area/b2b", "/fi/client-area/b2b","/general/client-area/portal/b2b/"],
-    "to": "/general/portal/b2b/"
+    from: [
+      "/en/client-area/b2b",
+      "/fi/client-area/b2b",
+      "/general/client-area/portal/b2b/",
+    ],
+    to: "/general/portal/b2b/",
   },
   {
-    "from": ["/en/client-area/services/", "/fi/client-area/services/","/general/client-area/portal/service-management/"],
-    "to": "/general/portal/service-management/"
+    from: [
+      "/en/client-area/services/",
+      "/fi/client-area/services/",
+      "/general/client-area/portal/service-management/",
+    ],
+    to: "/general/portal/service-management/",
   },
   {
-    "from": ["/en/game/rust/", "/fi/game/rust/"],
-    "to": "/game/rust/"
+    from: ["/en/game/rust/", "/fi/game/rust/"],
+    to: "/game/rust/",
   },
   {
-    "from": ["/en/game/minecraft/", "/fi/game/minecraft/"],
-    "to": "/game/minecraft/"
+    from: ["/en/game/minecraft/", "/fi/game/minecraft/"],
+    to: "/game/minecraft/",
   },
   {
-    "from": ["/en/game/minecraft/plugins", "/fi/game/minecraft/plugins"],
-    "to": "/game/minecraft/"
+    from: ["/en/game/minecraft/plugins", "/fi/game/minecraft/plugins"],
+    to: "/game/minecraft/",
   },
   {
-    "from": ["/en/game/minecraft/bungeecord", "/fi/game/minecraft/bungeecord"],
-    "to": "/game/minecraft/"
+    from: ["/en/game/minecraft/bungeecord", "/fi/game/minecraft/bungeecord"],
+    to: "/game/minecraft/",
   },
   {
-    "from": ["/en/game/gamepanel/desktop/", "/fi/game/gamepanel/desktop/"],
-    "to": "/general/desktop/"
+    from: ["/en/game/gamepanel/desktop/", "/fi/game/gamepanel/desktop/"],
+    to: "/general/desktop/",
   },
-]
+];
 
 module.exports = redirects;
